9d53e7bff44568ab817479301747e0e02a254be6
Nigel Babu <nigel@strapp.net>
2010-10-08 02:10:05 +0530

    Initial commit for code impact data handler

---
+++ log.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import csv
+import re
+import subprocess
+import sys
+
+def getlog(path):
+    log = "git log --shortstat -C --date=short"
+    p = subprocess.Popen(log, shell=True, cwd=path, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    output, errors = p.communicate()
+    return output
+    
+def splitinto(log):
+    expression = re.compile('^commit (.*)\sAuthor: (.*)\sDate:   (.*)\s*.*\s*(.*) files changed, (.*) insertions\(\+\), (.*) deletions', re.M)
+    result = re.findall(expression, log)
+    return result
+
+def export(stats):
+    statswriter = csv.writer(open('stats.csv', 'wb'))
+    for row in stats:
+        statswriter.writerow(row)
+
+def main():
+    log = getlog('/opt/lampp/htdocs/ibnetwork')
+    stats = splitinto(log)
+    export(stats)
+    
+if __name__ == "__main__":
+    sys.exit(main())
+    try:
+        sys.exit(main())
+    except Exception, e:
+        print "%s: %s" %(e.__class__.__name__, e)
+        sys.exit(1)
+if __name__ == "__main__":
+    main()
+    
